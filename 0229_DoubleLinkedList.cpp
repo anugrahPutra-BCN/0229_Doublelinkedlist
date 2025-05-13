0652bc1a1335d7e441db8db0570a1003dc170370
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-05-13 10:19:04 +0700

    deklarasi noMhs dan name untuk menampung data

--- 0229_DoubleLinkedList.cpp
+++ 0229_DoubleLinkedList.cpp
@@ -3,3 +3,8 @@ using namespace std;
 
 // membuat  struktur umtuk object double LinkedList
 struct Node
+{
+    // deklarasi noMhs dan name untuk menampung data 
+    int noMhs;
+    string name;
+  
