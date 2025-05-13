212166768001386538a122edc06d4415ea9765bd
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-05-13 12:10:52 +0700

    perulangan

--- 0229_DoubleLinkedList.cpp
+++ 0229_DoubleLinkedList.cpp
@@ -202,4 +202,50 @@ void searchData()
     }
 }
 
-
+int main()
+{
+    // perulangan selama bernilai benar untuk program utama double LinkedList
+    while (true)
+    {
+        try
+        {
+            cout << "\nMenu" << endl;
+            cout << "1. Add ad record to the list" << endl;
+            cout << "2. Delete a record from the list" << endl;
+            cout << "3. view all records in the ascending order of roll numbers" << endl;
+            cout << "4. view all records in the descending order of roll numbers" << endl;
+            cout << "5. Search for a record in the list" << endl;
+            cout << "6. Exit" << endl;
+            cout << "\nEnter your choice (1-6):" << endl;
+            char ch;
+            cin >> ch;
+             switch (ch)
+            {
+            case '1':
+                addNode();
+                break;
+            case '2':
+                deleteNode();
+                break;
+            case '3':
+                revtraverse();
+                break;
+            case '4':
+                revtraverse();
+                break;
+            case '5':
+                searchData();
+                break;
+            case '6':
+                return 0;
+            default:
+                cout << "\nInvalid option" << endl;
+                break;
+            }
+        }
+         catch (exception& e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
+    }
+}
